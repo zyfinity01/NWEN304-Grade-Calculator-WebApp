2259e48c73411958b8ce13c1958ba9b4bdd26e97
Lawrence <schwablawr@myvuw.ac.nz>
2023-09-12 16:33:55 +1200

    resolves #1, basic UI created.

--- group-project/src/App.tsx
+++ group-project/src/App.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// GradeCalculator component handles the grade calculation logic
+function GradeCalculator({ score }) {
+  let grade = 'Invalid input';
+
+  if (!isNaN(score)) {
+    const numericScore = parseFloat(score);
+    if (numericScore >= 90) {
+      grade = 'A';
+    } else if (numericScore >= 80) {
+      grade = 'B';
+    } else if (numericScore >= 70) {
+      grade = 'C';
+    } else if (numericScore >= 60) {
+      grade = 'D';
+    } else {
+      grade = 'F';
+    }
+  }
+
+  return (
+    <div className="Grade-result">
+      {grade && (
+        <p>Your grade is: <strong>{grade}</strong></p>
+      )}
+    </div>
+  );
+}
+
+// App component handles the UI
 function App() {
+  const [score, setScore] = React.useState('');
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Test
-        </a>
+        <h1>Grade Calculator</h1>
+        <div className="Grade-input">
+          <label htmlFor="score">Enter your score:</label>
+          <input
+            type="number"
+            id="score"
+            value={score}
+            onChange={(e) => setScore(e.target.value)}
+          />
+        </div>
+        <GradeCalculator score={score} />
       </header>
     </div>
   );
