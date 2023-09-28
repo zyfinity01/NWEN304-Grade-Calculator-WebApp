a6b437935a22f5b9e91ec1b28fa698f68b2c66c1
Niraj Gandhi <gandhinira@myvuw.ac.nz>
2023-09-28 23:18:45 +1300

    Implement basic rough login page

---
+++ group-project/src/login.js
@@ -0,0 +1,12 @@
+// src/Login.js
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <a href="http://localhost:3001/auth/google">Login with Google</a>
+        </div>
+    );
+}
+
+export default Login;
