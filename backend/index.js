05973a94b9df31f50d5ef9e05d320da9ffe9413d
Zoe Picone <zoe@zoepicone.dev>
2023-10-03 00:03:29 +1300

    Enable use of .local.env

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+const dotenv = require('dotenv')
+dotenv.config()
+dotenv.config({ path: `.local.env`, override: true });
 const cookieSession = require("cookie-session");
 const express = require("express");
 const cors = require("cors");
