c350c54038f7e2306b89d39a707ff61404649b7a
Niraj Gandhi <ngandhi360@gmail.com>
2023-10-19 20:52:47 +1300

    Change UI and fix env variable usage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,129 +1,142 @@
-// Home.js
 import React, { useState, useEffect } from 'react';
-
-import "./Home.css";
-
+import './Home.css';
 
 const Home = () => {
-    const [course, setCourse] = useState('');
-    const [grade, setGrade] = useState('');
-    const [currentUser, setCurrentUser] = useState(null);
+  const [courses, setCourses] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [courseName, setCourseName] = useState('');
+  const [assignments, setAssignments] = useState([]);
+  const [targetGrade, setTargetGrade] = useState('');
 
-    useEffect(() => {
-        // Fetch the current user data once the component is mounted
-        fetch('http://localhost:5000/currentUser', {
-            credentials: 'include'
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                setCurrentUser(data.username);
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error.message);
-            });
-
-    }, []);
-
-    const handleGradeCalculation = () => {
-        console.log('Calculating grades...');
-        const data = {
-            studentUsername: currentUser,
-            course: course,
-            score: parseFloat(grade)
-        };
-
-        fetch('http://localhost:5000/calculate', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.message);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    };
-
-
-
-    const handleSaveGrades = () => {
-
-        if (!currentUser) {
-            console.error('No user logged in.');
-            return;
+  // Fetch user on component mount
+  useEffect(() => {
+    fetch('${process.env.REACT_APP_BACKEND_API_URL}currentUser', {
+      credentials: 'include',
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
+        return response.json();
+      })
+      .then((data) => {
+        setCurrentUser(data.username);
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error.message);
+      });
+  }, []);
 
-        console.log('Saving grades...');
-
-
-        const data = {
-            studentUsername: currentUser,
-            course: {
-                courseName: course,
-                grade: parseFloat(grade)
-            }
-        };
-
-        fetch('http://localhost:5000/saveGrade', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.message);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+  const handleAddCourse = () => {
+    const data = {
+      studentUsername: currentUser,
+      course: {
+        courseName: courseName,
+        assignments: assignments,
+      },
     };
 
+    fetch('${process.env.REACT_APP_BACKEND_API_URL}saveCourse', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.message);
+        // Clear modal content and fetch updated courses
+        setCourseName('');
+        setAssignments([]);
+        setShowModal(false);
+        fetchCourses();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
 
-    return (
-        <div className="homeContainer">
-            <h1>Welcome to the VUW Grade Calculator</h1>
+  const fetchCourses = () => {
+    // Fetch courses for the user and update the `courses` state.
+    // Placeholder functionality.
+  };
 
-            <div className="gradeCalculationUI">
-                <div className="courseAndButton">
-                    <input
-                        type="text"
-                        value={course}
-                        onChange={e => setCourse(e.target.value)}
-                        placeholder="Enter Course"
-                        className="courseInput"
-                    />
+  const addAssignment = () => {
+    setAssignments([...assignments, { name: '', weight: 0, score: 0 }]);
+  };
 
-                </div>
-                <input
-                    type="text"
-                    value={grade}
-                    onChange={e => setGrade(e.target.value)}
-                    placeholder="Enter Grade"
-                    className="gradeInput"
-                />
-                <button onClick={handleSaveGrades} className="calculateBtn">
-                    Save Grades
-                </button>
+  const handleAssignmentChange = (index, field, value) => {
+    const newAssignments = [...assignments];
+    newAssignments[index][field] = value;
+    setAssignments(newAssignments);
+  };
 
-                <button onClick={handleGradeCalculation} className="calculateBtn">
-                    Calculate Grades - test
-                </button>
+  return (
+    <div className="homeContainer">
+      <h1>Welcome to the VUW Grade Calculator</h1>
 
+      <button onClick={() => setShowModal(true)}>Add New Course</button>
 
+      {showModal && (
+        <div className="modal">
+          <input
+            type="text"
+            value={courseName}
+            onChange={(e) => setCourseName(e.target.value)}
+            placeholder="Course Name"
+          />
+
+          {assignments.map((assignment, index) => (
+            <div key={index}>
+              <input
+                value={assignment.name}
+                onChange={(e) => handleAssignmentChange(index, 'name', e.target.value)}
+                placeholder="Assignment Name"
+              />
+              <input
+                type="number"
+                value={assignment.weight}
+                onChange={(e) => handleAssignmentChange(index, 'weight', +e.target.value)}
+                placeholder="Weight"
+              />
+              <input
+                type="number"
+                value={assignment.score}
+                onChange={(e) => handleAssignmentChange(index, 'score', +e.target.value)}
+                placeholder="Score"
+              />
             </div>
+          ))}
+
+          <button onClick={addAssignment}>Add Assignment/Exam</button>
+          <button onClick={handleAddCourse}>Save Course</button>
+          <button onClick={() => setShowModal(false)}>Cancel</button>
         </div>
-    );
+      )}
+
+      <div className="coursesDisplay">
+        {/* Display courses, average grades, and target grades */}
+        {courses.map((course) => (
+          <div key={course.id}>
+            <h2>{course.courseName}</h2>
+            {/* Display average grade and target grade logic here */}
+          </div>
+        ))}
+      </div>
+
+      <div className="targetGradeUI">
+        <input
+          type="number"
+          value={targetGrade}
+          onChange={(e) => setTargetGrade(e.target.value)}
+          placeholder="Desired Average"
+        />
+        {/* <button onClick={Fetch or calculate target grade logic here}>Calculate</button> */}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
