d91d85172c8362db6abac40a0c846156587cf326
Lawrence <schwablawr@myvuw.ac.nz>
2023-10-23 19:40:08 +1300

    Updated home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,40 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './Home.css';
-// import Slider from "react-slick"; // Import the Slider component
-
 
 const Home = () => {
 
-  const mockCourses = [
-    {
-      name: 'Math 101',
-      assignments: [
-        { name: 'Assignment 1', grade: 80, weight: 10 },
-        { name: 'Exam 1', grade: 90, weight: 40 },
-      ],
-      average: 85,
-    },
-    {
-      name: 'Physics 202',
-      assignments: [
-        { name: 'Lab 1', grade: 75, weight: 15 },
-        { name: 'Midterm Exam', grade: 88, weight: 30 },
-      ],
-      average: 81,
-    },
-    // ... add more mock course data here ...
-  ];
-
-  const [courses, setCourses] = useState(mockCourses); // Initialize with mock data
+  const [courses, setCourses] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [courseName, setCourseName] = useState('');
   const [assignments, setAssignments] = useState([]);
-  //const [targetGrade, setTargetGrade] = useState('');
 
-
-
-  // Fetch user on component mount
   useEffect(() => {
     fetch(`${process.env.REACT_APP_BACKEND_API_URL}currentUser`, {
       credentials: 'include',
@@ -48,20 +22,21 @@ const Home = () => {
       .then((data) => {
         setCurrentUser({
           username: data.username,
-          studentId: data._id // assuming the backend sends the _id as "_id"
+          studentId: data._id
         });
       })
       .catch((error) => {
         console.error('There was a problem with the fetch operation:', error.message);
       });
-  },
-  []);
+  }, []);
 
   const fetchCourses = () => {
-    fetch(`${process.env.REACT_APP_BACKEND_API_URL}getAllCourses`)
+    fetch(`${process.env.REACT_APP_BACKEND_API_URL}getAllCourses`, {
+      credentials: 'include',
+      })
       .then((response) => response.json())
       .then((fetchedCourses) => {
-        setCourses(prevCourses => [...prevCourses, ...fetchedCourses]); // merging previous courses with the fetched ones
+        setCourses(fetchedCourses);
       })
       .catch((error) => {
         console.error('There was a problem with the fetch operation:', error.message);
@@ -125,6 +100,7 @@ const Home = () => {
         addAssignments(courseName, assignments);
         setCourseName('');
         setShowModal(false);
+        fetchCourses();
       });
   };
 
@@ -138,82 +114,10 @@ const Home = () => {
     setAssignments(newAssignments);
   };
 
-  // const settings = {
-  //   dots: true,
-  //   infinite: true,
-  //   speed: 500,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1
-  // };
-
-  // const handleAddCourse = () => {
-  //   const courseData = {
-  //     courseName: courseName,
-  //     pointValue: 15
-  //   };
-
-  //   fetch(`${process.env.REACT_APP_BACKEND_API_URL}addCourse`, {
-  //     credentials: 'include',
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(courseData),
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log(data.message);
-  //       // Clear modal content and fetch updated courses
-  //       setCourseName('');
-  //       setShowModal(false);
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error:', error);
-  //     });
-
-  //   assignments.forEach(assignment => {
-  //     const assignmentData = {
-  //       courseName: courseName, // referencing the course name for each assignment
-  //       name: assignment.name,
-  //       weight: assignment.weight,
-  //       grade: assignment.score 
-  //     };
-
-  //     fetch(`${process.env.REACT_APP_BACKEND_API_URL}addAssignment`, {
-  //       credentials: 'include',
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(assignmentData),
-  //     })
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         console.log(data.message); // log the response from the server for adding the assignment
-  //       })
-  //       .catch((error) => {
-  //         console.error('Error:', error);
-  //       });
-  //   });
-  // };
-
-
-  // const addAssignment = () => {
-  //   setAssignments([...assignments, { name: '', weight: 0, score: 0 }]);
-  // };
-
-  // const handleAssignmentChange = (index, field, value) => {
-  //   const newAssignments = [...assignments];
-  //   newAssignments[index][field] = value;
-  //   setAssignments(newAssignments);
-  // };
-
   return (
     <div className="homeContainer">
       <h1>Welcome to the VUW Grade Calculator</h1>
-
       <button className="addCourseBtn" onClick={() => setShowModal(true)}>Add New Course</button>
-
       {showModal && (
         <div className="modal">
           <input
@@ -222,7 +126,6 @@ const Home = () => {
             onChange={(e) => setCourseName(e.target.value)}
             placeholder="Course Name"
           />
-
           {assignments.map((assignment, index) => (
             <div key={index}>
               <input
@@ -244,13 +147,11 @@ const Home = () => {
               />
             </div>
           ))}
-
           <button onClick={addAssignment}>Add Assignment/Exam</button>
           <button onClick={handleAddCourse}>Save Course</button>
           <button onClick={() => setShowModal(false)}>Cancel</button>
         </div>
       )}
-
       <div className="coursesDisplay">
         {courses.map((course, index) => (
           <div key={index} className="courseCard">
@@ -267,12 +168,9 @@ const Home = () => {
         ))}
       </div>
       <div className="resultsBox">
-        {/* This is the new results box on the right-hand side */}
         <h3>Grade Calculation Results</h3>
-        {}
       </div>
       <button className="fetchCourseBtn" onClick={fetchCourses}>Fetch Courses</button>
-
     </div>
   );
 };
